ec23998c72b37591297da924a802ec55854adf60
Abu Sadat Md Sayem <sadat.talks@gmail.com>
2021-01-17 00:57:41 +0600

    saving

--- src/components/Admin.vue
+++ src/components/Admin.vue
@@ -163,17 +163,10 @@
       </template>
       <template v-slot:item.actions="{ item }">
         <v-icon
-          small
-          class="mr-2"
+          class="ml-2"
           @click="editItem(item)"
         >
           mdi-pencil
-        </v-icon>
-        <v-icon
-          small
-          @click="deleteItem(item)"
-        >
-          mdi-delete
         </v-icon>
       </template>
       <template v-slot:no-data>
--- src/store/modules/admin.js
+++ src/store/modules/admin.js
@@ -3,6 +3,7 @@ import instance from '../../helpers/axios';
 export default {
   state: {
     admins: [],
+    profile: {},
   },
   mutations: {
     SET_ADMINS(state, admins) {
@@ -10,6 +11,9 @@ export default {
     },
     ADD_ADMIN(state, admin) {
       state.admins = [admin, ...state.admins];
+    },
+    SET_ADMIN_PROFILE(state, data) {
+      state.profile = data;
     },
     UPDATE_ADMIN(state, admin) {
       const index = state.admins.findIndex((a) => a.id === admin.id);
@@ -39,10 +43,18 @@ export default {
         return Promise.reject(err);
       }
     },
+    async ADMIN_PROFILE_REQUEST({ commit }) {
+      try {
+        const { data } = await instance.get('/admin/profile');
+        commit('SET_ADMIN_PROFILE', data?.data);
+        return data;
+      } catch (err) {
+        return Promise.reject(err);
+      }
+    },
     async ADD_ADMIN_REQUEST({ commit }, payload) {
       try {
         const { data } = await instance.post('/admin/add', payload);
-        console.log(data?.data);
         commit('ADD_ADMIN', data?.data);
         return data;
       } catch (err) {
@@ -52,5 +64,6 @@ export default {
   },
   getters: {
     Admins: (state) => state.admins,
+    AdminProfile: (state) => state.profile,
   },
 };
